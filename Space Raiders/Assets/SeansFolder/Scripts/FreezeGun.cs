f09653eec58016ffd4ea56b560b95ff05d904613
Sean <seangullery@gmail.com>
2021-04-16 15:24:48 +1200

    Freeze Gun Sean

---
+++ Space Raiders/Assets/SeansFolder/Scripts/FreezeGun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeGun : MonoBehaviour
+{
+    public GameObject freezebeam;
+    public bool freezing;
+    public Rigidbody projectile;
+    public Transform gunpoint;
+    public LayerMask ground;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        freezebeam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            freezebeam.SetActive(true);
+            freezing = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            freezebeam.SetActive(false);
+            freezing = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && !freezing)
+        {
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectile, gunpoint.position, gunpoint.rotation) as Rigidbody;
+            projectileInstance.AddForce(gunpoint.forward * 4000f);
+
+
+        }
+    }
+
+}
---
+++ Space Raiders/Assets/ballRemove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballRemove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3.5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
