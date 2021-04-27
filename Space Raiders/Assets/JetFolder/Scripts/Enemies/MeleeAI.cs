2534a0c6454f27a0c7e3a61797d224f49059d66c
Jet Yong <jettlag2000@gmail.com>
2021-04-27 15:25:42 +1200

    Waypoints

---
+++ Space Raiders/Assets/JetFolder/Scripts/Enemies/MeleeAI.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+public class MeleeAI : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    [SerializeField]
+    private NPCState npcState = NPCState.idle;
+
+    public GameObject Player;
+
+    [SerializeField]
+    float wanderDist = 10f;
+
+    public bool seenPlayer;
+    public float speed;
+
+    public enum NPCState
+    {
+        idle,
+        wander,
+        chase,
+        attack,
+    }
+
+    void Update()
+    {
+        switch (npcState)
+        {
+            case NPCState.idle:
+                Idle();
+
+                break;
+            case NPCState.wander:
+                Wander();
+
+                break;
+            case NPCState.chase:
+                Chase();
+
+                break;
+            case NPCState.attack:
+                Attack();
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    void Idle()
+    {
+        if(seenPlayer == false)
+        {
+
+        }
+    }
+
+    void Wander()
+    {
+        if(seenPlayer == false)
+        {
+
+        }
+    }
+
+    void Chase()
+    {
+        if(seenPlayer == true)
+        {
+
+        }
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Death()
+    {
+
+    }
+}
