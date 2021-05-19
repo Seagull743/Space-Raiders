e95d5ed51d5f89ff0cc0cf27771d030e94b93ef9
Sean <seangullery@gmail.com>
2021-05-19 12:47:21 +1200

    Health Bar For Ai

--- Space Raiders/Assets/SeansFolder/Scripts/Health.cs
+++ Space Raiders/Assets/SeansFolder/Scripts/Health.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
 
     public float maxHealth = 100;
     public float currenthealth;
+
+    [SerializeField]
+    private Image EnemyHealthBar;
+   
+ 
     
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         currenthealth = maxHealth;
+        EnemyHealthBar.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        EnemyHealthBar.fillAmount = currenthealth / maxHealth;
+        
+        
+        
         if(currenthealth <= 0)
         {
             this.gameObject.SetActive(false);
         }
+    
     }
 
     public void RespawnEnemy()
@@ -35,6 +48,16 @@ public class Health : MonoBehaviour
 
     public void ShardDamage(int damageAmount)
     {
-        currenthealth -= damageAmount;
+        currenthealth -= damageAmount;        
+        StartCoroutine(HealthBarIndication());
     }
+
+
+    IEnumerator HealthBarIndication()
+    {
+        EnemyHealthBar.enabled = true;
+        yield return new WaitForSeconds(3);
+        EnemyHealthBar.enabled = false;  
+    }
+
 }
---
+++ Space Raiders/Assets/SeansFolder/Scripts/PlayerIntetaction/YellowCrystalShrine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowCrystalShrine : InteractiveObject
+{
+    public GameObject Crystal;
+    public static bool YellowCrystalplaced = false;
+
+    public override void PlayerInteraction()
+    {
+        PlaceCrystal();
+    }
+
+    public void PlaceCrystal()
+    {
+        if (YellowCrystal.YellowCrystalCollected == true)
+        {
+            Crystal.SetActive(true);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            YellowCrystalplaced = true;
+        }
+        else
+        {
+            GameManager.CrystalText();
+        }
+    }
+}
