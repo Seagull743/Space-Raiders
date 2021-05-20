9401c90175990d488a5b1297433e78b819454cf0
Jet Yong <jettlag2000@gmail.com>
2021-05-20 13:03:59 +1200

    overridedenemyscales

--- Space Raiders/Assets/JetFolder/Scripts/Endgame.cs
+++ Space Raiders/Assets/JetFolder/Scripts/Endgame.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Endgame : MonoBehaviour
 {
+    public bool green;
+    public bool red;
+    public bool purple;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        green = GreenCrystalShrine.GreenPlaced;
+        red = RedCrystalShrine.redCrystalplaced;
+        purple = PurpleCrystalShrine.PurplePlaced;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (green == true && red == true && purple == true)
+        {
+            
+        }
     }
 }
