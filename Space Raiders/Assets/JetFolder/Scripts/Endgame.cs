247a59914453fc76335f369937269249727322b2
Jet Yong <jettlag2000@gmail.com>
2021-05-20 14:48:49 +1200

    EnemyLayerMask

--- Space Raiders/Assets/JetFolder/Scripts/Endgame.cs
+++ Space Raiders/Assets/JetFolder/Scripts/Endgame.cs
@@ -5,24 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Endgame : MonoBehaviour
 {
-    public bool green;
-    public bool red;
-    public bool purple;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        green = GreenCrystalShrine.GreenPlaced;
-        red = RedCrystalShrine.redCrystalplaced;
-        purple = PurpleCrystalShrine.PurplePlaced;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (green == true && red == true && purple == true)
+        if (GreenCrystalShrine.GreenPlaced == true && RedCrystalShrine.redCrystalplaced == true && PurpleCrystalShrine.PurplePlaced == true)
         {
-            
+            SceneManager.LoadScene("JetScene");
         }
     }
 }
---
+++ Space Raiders/Assets/JetFolder/Scripts/SoundCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollider : MonoBehaviour
+{
+    [SerializeField]
+    float radius;
+
+    [SerializeField]
+    LayerMask Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(this.transform.position, radius, Enemy);
+
+        foreach(var enemyInRange in enemiesInRange)
+        {
+
+        }
+    }
+}
