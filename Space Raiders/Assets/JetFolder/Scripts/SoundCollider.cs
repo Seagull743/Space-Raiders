741be64e49996ee8476f9e75dbfe75c6a5dc5026
Al <theonlypurplecat@gmail.com>
2021-05-24 13:11:34 +1200

    Added in gun-tex + prefabbed all Hud Components

---
+++ Space Raiders/Assets/JetFolder/Scripts/SoundCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollider : MonoBehaviour
+{
+    [SerializeField]
+    float radius;
+
+    [SerializeField]
+    LayerMask Enemy;
+
+    public bool alert;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alert = GetComponent<EnemyAI>().alerted;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(this.transform.position, radius, Enemy);
+
+        foreach(var enemyInRange in enemiesInRange)
+        {
+            alert = true;
+        }
+    }
+}
---
+++ Space Raiders/Assets/SeansFolder/Scripts/BossSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bossSpawn;
+
+    
+    void Update()
+    {
+        if (GreenCrystalShrine.GreenPlaced == true && PurpleCrystalShrine.PurplePlaced == true && RedCrystalShrine.redCrystalplaced == true)
+        {
+            GameManager.BossSpawn(bossSpawn);            
+        }
+    }
+}
