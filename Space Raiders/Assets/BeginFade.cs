c6d9cf550b15b6cdf8b2b82611367f4060a101b0
Iaon <peetthunpisit@gmail.com>
2021-05-26 16:19:33 +1200

    MainMenu/Lose/Win Scene Finish

---
+++ Space Raiders/Assets/BeginFade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginFade : MonoBehaviour
+{
+    [SerializeField] Animator anim = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn()
+    {
+        anim.SetBool("in", true);
+    }
+  
+
+    public void FadeOut()
+    {
+        anim.SetBool("out", true);
+        anim.SetBool("in", false);
+    }
+
+    public void FadeOutOff()
+    {
+        anim.SetBool("out", false);
+    }
+}
