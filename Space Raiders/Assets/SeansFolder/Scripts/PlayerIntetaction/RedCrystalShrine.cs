43c6ab9a6d3f70a38b47e1eec014053615577594
Al <theonlypurplecat@gmail.com>
2021-06-04 16:12:13 +1200

    small fixes - mouse sensitivity halved, bullet life shortened

---
+++ Space Raiders/Assets/SeansFolder/Scripts/PlayerIntetaction/RedCrystalShrine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCrystalShrine : InteractiveObject
+{
+    public GameObject Crystal;
+    public static bool redCrystalplaced = false;
+
+    public override void PlayerInteraction()
+    {
+        PlaceCrystal();
+    }
+
+    public void PlaceCrystal()
+    {
+        if (RedCrystal.RedCrystalCollected == true)
+        {
+            Crystal.SetActive(true);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            redCrystalplaced = true;
+        }
+        else
+        {
+            GameManager.CrystalText();
+        }
+    }
+}
